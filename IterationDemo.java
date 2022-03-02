377ba02a9adacf3675a431667688dbd558b279d0
dineshmuniraj <67988746+dineshmuniraj@users.noreply.github.com>
2022-03-02 02:31:03 -0800

    Add files via upload
    to create the pattern
    A
    A B
    A B C

---
+++ IterationDemo.java
@@ -0,0 +1,12 @@
+public class IterationDemo {
+    public static void main(String[] args) {
+        for(int i =65;i<=68;i++)
+        {
+        for(int j = 65;j<=i;j++)
+            {
+                System.out.print((char)j + " ");
+            }
+        System.out.println();
+        }
+    }
+}
